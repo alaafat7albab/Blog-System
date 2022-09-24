cff8e60278480954b159108b4ce343cb24626a43
alaafat7albab <alaa.fathalbab@gmail.com>
2022-09-24 21:35:43 +0200

    saving data to database

--- form.php
+++ form.php
@@ -1,3 +1,13 @@
+<?php
+    //===============================================================
+    //=================Check For Errors==============================
+    //===============================================================
+    $error_array = array();
+    if($_GET['error_fields']){
+        $error_array = explode(',', $_GET['error_fields']);
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -7,9 +17,9 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-        <!-- Body of Form starts -->
+     <!-- Body of Form starts -->
         <div class="container">
-            <form method="post" autocomplete="on">
+            <form action="./process.php" method="post">
                 <!--First name-->
                 <div class="box">
                     <label for="firstName" class="fl fontLabel"> First Name: </label>
@@ -18,27 +28,37 @@
                     </div>
                     <div class="fr">
                         <label>
-                            <input type="text" name="firstName" placeholder="First Name"
-                                   class="textBox" autofocus="on" required>
+                            <input type="text" name="first_name" placeholder="First Name"
+                                   class="textBox">
                         </label>
                     </div>
                     <div class="clr"></div>
                 </div>
+                <?php
+                    if(in_array("first_name", $error_array)){
+                        echo "<p style='color: red; padding: 2px'>* Please Enter First Name</p>";
+                    }
+                ?>
                 <!--First name-->
 
 
                 <!--Second name-->
                 <div class="box">
-                    <label for="secondName" class="fl fontLabel"> Seconed Name: </label>
+                    <label for="secondName" class="fl fontLabel"> Second Name: </label>
                     <div class="fl iconBox"><i class="fa fa-user" aria-hidden="true"></i></div>
                     <div class="fr">
                         <label>
-                            <input type="text" required name="secondName"
+                            <input type="text"  name="last_name"
                                    placeholder="Last Name" class="textBox">
                         </label>
                     </div>
                     <div class="clr"></div>
                 </div>
+                <?php
+                if(in_array("last_name", $error_array)){
+                    echo "<p style='color: red; padding: 2px'>* Please Enter Second Name</p>";
+                }
+                ?>
                 <!--Second name-->
 
 
@@ -48,11 +68,16 @@
                     <div class="fl iconBox"><i class="fa fa-phone-square" aria-hidden="true"></i></div>
                     <div class="fr">
                         <label>
-                            <input type="text" required name="phoneNo" maxlength="10" placeholder="Phone No." class="textBox">
+                            <input type="text"  name="phone_no" maxlength="10" placeholder="Phone No." class="textBox">
                         </label>
                     </div>
                     <div class="clr"></div>
                 </div>
+                <?php
+                if(in_array("phone_no", $error_array)){
+                    echo "<p style='color: red; padding: 2px'>* Please Enter Phone Number</p>";
+                }
+                ?>
                 <!---Phone No.---->
 
 
@@ -62,11 +87,16 @@
                     <div class="fl iconBox"><i class="fa fa-envelope" aria-hidden="true"></i></div>
                     <div class="fr">
                         <label>
-                            <input type="email" required name="email" placeholder="Email Id" class="textBox">
+                            <input type="email"  name="email" placeholder="Email Id" class="textBox">
                         </label>
                     </div>
                     <div class="clr"></div>
                 </div>
+                <?php
+                if(in_array("email", $error_array)){
+                    echo "<p style='color: red; padding: 2px'>* Please Enter Valid Email</p>";
+                }
+                ?>
                 <!--Email ID----->
 
 
@@ -76,21 +106,26 @@
                     <div class="fl iconBox"><i class="fa fa-key" aria-hidden="true"></i></div>
                     <div class="fr">
                         <label>
-                            <input type="Password" required name="password" placeholder="Password" class="textBox">
+                            <input type="Password"  name="password" placeholder="Password" class="textBox">
                         </label>
                     </div>
                     <div class="clr"></div>
                 </div>
+                <?php
+                if(in_array("password", $error_array)){
+                    echo "<p style='color: red; padding: 2px'>* Please Enter password Greater than 5</p>";
+                }
+                ?>
                 <!---Password---->
 
                 <!---Gender----->
                 <div class="box radio">
                     <label for="gender" class="fl fontLabel"> Gender: </label>
                     <label>
-                        <input type="radio" name="Gender" value="Male" required>
+                        <input type="radio" name="gender" value="male" >
                     </label> Male &nbsp; &nbsp; &nbsp; &nbsp;
                     <label>
-                        <input type="radio" name="Gender" value="Female" required>
+                        <input type="radio" name="gender" value="female" >
                     </label> Female
                 </div>
                 <!---Gender--->
@@ -99,7 +134,7 @@
                 <!--Terms and Conditions------>
                 <div class="box terms">
                     <label>
-                        <input type="checkbox" name="Terms" required>
+                        <input type="checkbox" name="Terms" >
                     </label> &nbsp; I accept the terms and conditions
                 </div>
                 <!--Terms and Conditions------>
---
+++ process.php
@@ -0,0 +1,69 @@
+<?php
+
+    //===============================================================
+    //========================Debugger===============================
+    //===============================================================
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    //===============================================================
+    //====================Validation=================================
+    //===============================================================
+    $error_fields = array();
+    if (! (isset($_POST['first_name']) && !empty($_POST['first_name']))){
+        $error_fields[] = "first_name";
+    }
+    if (! (isset($_POST['last_name']) && !empty($_POST['last_name']))){
+        $error_fields[] = "last_name";
+    }
+    if (! (isset($_POST['phone_no']) &&  !is_int($_POST['phone_no']))){
+        $error_fields[] = "phone_no";
+    }
+    if (! (isset($_POST['email']) && filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL ))){
+        $error_fields[] = "email";
+    }
+    if (! (isset($_POST['password']) && strlen($_POST['password']) > 5)){
+        $error_fields[] = "password";
+    }
+    if($error_fields){
+        header("Location: form.php?error_fields=" . implode(',', $error_fields));
+        exit;
+    }
+
+    //=======================================================================
+    //=======================Open The Connection=============================
+    //=======================================================================
+    $conn = mysqli_connect("localhost", "alaa", "", "blog");
+    if(! $conn){
+        mysqli_connect_error($conn);
+        exit;
+    }
+
+    //===============================================================================================
+    //==============Escape any special characters to avoid the sql injection=========================
+    //===============================================================================================
+    $firstName  =   mysqli_escape_string($conn, $_POST['first_name']);
+    $lastName   =   mysqli_escape_string($conn, $_POST['last_name']);
+    $phoneNo    =   mysqli_escape_string($conn, $_POST['phone_no']);
+    $email      =   mysqli_escape_string($conn, $_POST['email']);
+    $password   =   mysqli_escape_string($conn, $_POST['password']);
+
+    //=================================================================
+    //======================Insertion==================================
+    //=================================================================
+    $query  = "INSERT INTO `users`  (`first_name`, `last_name`, `phone_no`, `email`, `password`, `gender`) 
+                VALUES ('".$firstName."', '".$lastName."', ".$phoneNo.", '".$email."', sha1('".$password."'), '".$_POST['gender']."')";
+
+    if(mysqli_query($conn, $query)) {
+        if ($_POST['gender'] == 'female') {
+            echo "<h1 style='color: deeppink'> Hello " . $_POST['first_name'] . "</h1>";
+        } else if ($_POST['gender'] == 'male') {
+            echo "<h1 style='color: blue'> Hello " . $_POST['first_name'] . "</h1>";
+        }
+    }
+    //===================================================================
+    //========================Close The Connection=======================
+    //===================================================================
+    mysqli_close($conn);
+
